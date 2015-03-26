ffc64d158f4d3d8d9a737aa2597531058fbf5750
Ynguibjoerg <Ynguibjoerg@users.noreply.github.com>
2015-03-26 18:56:24 +0100

    Hijacking index.php for some wild tests

--- web/index.php
+++ web/index.php
@@ -1,3 +1,31 @@
 <?php
-$app = require __DIR__.'/../app/app.php';
-$app->run();
+//$app = require __DIR__.'/../app/app.php';
+// $app->run();
+
+include 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+session_start();
+$_SESSION['host'] = "horst";
+try {
+  // specify where to look for templates
+  $loader = new Twig_Loader_Filesystem('../app/templates');
+  
+  // initialize Twig environment
+  $twig = new Twig_Environment($loader);
+  
+  // load template
+  $template = $twig->loadTemplate('index.twig');
+  
+  // set template variables
+  // render template
+  echo $template->render(array(
+    
+                'title'      => "Welcome",
+                'error'      => null,
+                'user'       => "root",
+                'host'       => $_SESSION['host'],
+  ));
+  
+} catch (Exception $e) {
+  die ('ERROR: ' . $e->getMessage());
+}
