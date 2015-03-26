8362259e596a09c65dad6e41c71ddf86788e3f04
mpatrick <melanie.patrick@trivago.com>
2015-03-26 19:14:48 +0100

    code formatting

--- web/databases.php
+++ web/databases.php
@@ -5,88 +5,61 @@
 require('../vendor/twig/twig/lib/Twig/Autoloader.php');
 Twig_Autoloader::register();
 session_start();
-$databases = getListOfDatabases();
 
-if(isset($_GET['database']){
-	  
-          // TODO if database list is empty: show warning and refuse further commandos
-
-    if (in_array($_REQUEST['database'], $databases))
-    {
-        $_SESSION['database'] = $_REQUEST['database'];
-        redirectTo("query.php");
-    }
-} else {
-
-
-try {
-  // specify where to look for templates
-  $loader = new Twig_Loader_Filesystem('../app/templates');
-  
-  // initialize Twig environment
-  $twig = new Twig_Environment($loader);
-  
-  // load template
-  $template = $twig->loadTemplate('databases.twig');
-  
-  $databases = null;
- require_once("list_databases.inc.php");
-
-  // set template variables
-  // render template
-  echo $template->render(array(
-    
-                'title'      => "Welcome",
-                'error'      => null,
-                'user'       => $_SESSION['user'],
-                'host'       => $_SESSION['host'],
-  ));
-  
-} catch (Exception $e) {
-  die ('ERROR: ' . $e->getMessage());
-}
-}
-function redirectTo($path){
-	header("Location: " . $path);
-die();
-}
-
-function getListOfDatabases()
+if ($_POST)
 {
-	$host = $session->get('host');
-    $url        = "http://" . $_SESSION['host'] . "/db/?u=" . $_SESSION['user'] . "&p=" . $_SESSION['pw'];
-    print ("URRREL " . $url);
-    $httpResult = getUrlContent($url);
+    $loggedIn      = false;
+    $error_message = null;
+    // session_start();
 
-    if (200 == $httpResult['status_code'])
+    require_once("../app/login.inc.php");
+
+    if ($loggedIn)
     {
-
-        $json = json_decode($httpResult['results']);
-        print_r($json);
-        $result = array();
-        foreach ($json as $key => $value)
-        {
-            $result[] = $value;
-        }
-
-        return $result;
+        redirectTo("databases.php");
     }
     else
     {
-        // TODO error handling
+        $_SESSION['host'] = "";
+        $_SESSION['user'] = "";
+        // does not redirect, will end up in loginform
     }
 }
-
-function getUrlContent($url)
+else
 {
-	    $ch = curl_init();
-	    curl_setopt($ch, CURLOPT_URL, $url);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-	    $data       = curl_exec($ch);
-	    $statuscode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	    curl_close($ch);
+    $_SESSION['host'] = "";
+    $_SESSION['user'] = "";
+}
 
-	    return ['status_code' => $statuscode, 'results' => $data];
+try
+{
+    // specify where to look for templates
+    $loader = new Twig_Loader_Filesystem('../app/templates');
+
+    // initialize Twig environment
+    $twig = new Twig_Environment($loader);
+
+    // load template
+    $template = $twig->loadTemplate('index.twig');
+
+    // set template variables
+    // render template
+    echo $template->render(
+        array(
+            'title' => "Welcome",
+            'error' => null,
+            'user'  => $_SESSION['user'],
+            'host'  => $_SESSION['host'],
+        )
+    );
+}
+catch (Exception $e)
+{
+    die ('ERROR: ' . $e->getMessage());
+}
+
+function redirectTo($path)
+{
+    header("Location: " . $path);
+    die();
 }
