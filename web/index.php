69e477227523c170d93ca5454698981193a6f4e6
Ynguibjoerg <Ynguibjoerg@users.noreply.github.com>
2015-03-26 19:20:12 +0100

    It is getting late

--- web/index.php
+++ web/index.php
@@ -12,7 +12,20 @@ if ($_POST)
     $error_message = null;
     // session_start();
 
-    require_once("app/login.inc.php");
+    if ($_POST)
+    {
+        $loggedIn = checkLoginValid();
+
+        if ($loggedIn)
+        {
+            storeToSession();
+            addLoginToCookie();
+        }
+        else
+        {
+            $error_message = "Invalid login";
+        }
+    }
 
     if ($loggedIn)
     {
@@ -63,4 +76,48 @@ function redirectTo($path)
 {
     header("Location: " . $path);
     die();
+}
+
+function checkLoginValid()
+{
+    $url        = "http://" . $_POST['host'] . "/db/?u=" . $_POST['user'] . "&p=" . $_POST['pw'];
+    $httpResult = getUrlContent($url);
+
+    return (200 == $httpResult['status_code']);
+}
+
+function storeToSession()
+{
+    $session->set('host', $_POST['host']);
+    $_SESSION['host'] = $_POST['host'];
+    $_SESSION['user'] = $_POST['user'];
+    $_SESSION['pw']   = $_POST['pw'];
+}
+
+
+function addLoginToCookie(){
+    $cookie_name = "last_logins";
+    $saveMe = $_SESSION['user'] . "@" . $_SESSION['host'];
+    $oldValue = readCookie($cookie_name);
+    $newValue = $oldValue . "|" . $saveMe;
+    setcookie($cookie_name, $newValue, time() + (86400 * 30), '/');
+}
+
+function readCookie($cookie_name){
+    return (isset($_COOKIE[$cookie_name])) ? $_COOKIE[$cookie_name] : "";
+
+}
+
+function getUrlContent($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    $data       = curl_exec($ch);
+    $statuscode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return ['status_code' => $statuscode, 'results' => $data];
 }
