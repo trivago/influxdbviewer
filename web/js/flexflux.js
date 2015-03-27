95c3beaf96633419ebc3d62a47a0efed155b3113
creinartz <creinartz@trivago-686.local>
2015-03-27 12:19:58 +0100

    logo

--- web/js/flexflux.js
+++ web/js/flexflux.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    //Ladda.bind( 'input[type=submit]', { timeout: 2000 } );
+
     var _sidebar = $('#js_sidebar');
     var _header = $('#js_sidebar header');
     var _section = $('.main-content');
