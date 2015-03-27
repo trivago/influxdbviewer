81db5d8dd3dc7196889d6f90e16ef2bd7c40eeba
creinartz <creinartz@trivago-686.local>
2015-03-27 12:24:46 +0100

    js changes

--- web/js/flexflux.js
+++ web/js/flexflux.js
@@ -10,24 +10,13 @@ $(document).ready(function() {
 
         if(!_sidebar.hasClass('disabled'))
         {
-            _sidebar.animate({
-                width: 20
-            }, 100, function() {
-                _sidebar.removeAttr('style');
-            });
             _sidebar.addClass('disabled');
             _section.removeClass('is-open')
         }
         else
         {
-            _sidebar.animate({
-                width: 250
-            }, 100, function() {
-                // Animation complete.
-                _section.addClass('is-open')
-                _sidebar.removeClass('disabled');
-                _sidebar.removeAttr('style');
-            });
+            _section.addClass('is-open')
+            _sidebar.removeClass('disabled');
         }
     });
 });
