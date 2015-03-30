de725d4ec0916d523ea040c7c3da7ab5ab77c845
Ynguibjoerg <Ynguibjoerg@users.noreply.github.com>
2015-03-30 16:24:53 +0200

    Debugging improved

--- web/func.inc.php
+++ web/func.inc.php
@@ -213,7 +213,7 @@ function getDatabaseResults($query)
         }
         else
         {
-        	debug("Error message! ");
+        	debug("Error message! Status code: " . $httpResult['status_code']);
         	$json = json_decode($httpResult['results']);
         	debug ($json);
         	$feedback['error_message'] = $json;
