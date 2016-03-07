547295062613b2207d2024cd5c33492942a3c0a5
Inga Feick <inga.feick@trivago.com>
2016-03-07 15:04:14 +0100

    Reorder all the code

--- web/renderobject.class.php
+++ web/renderobject.class.php
@@ -13,8 +13,7 @@ class Renderobject extends Resultset {
     #var $user = $_SESSION['user']; // TODO fix
     #var $host = $_SESSION['host']; // TODO fix
     #var $database = $_SESSION['database']; // TODO fix
-    var $results = null;
-	var $datapoints = [];
+    var $datapoints = [];
     var $columns = [];
     var $is_series_list = false; // TODO switch to enum
     var $start_pagination = 1;
